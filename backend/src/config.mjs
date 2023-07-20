75f90d834e81bf37f1d634287c33fce1b87d2d5b
Agustina-Flores <28354189+Agustina-Flores@users.noreply.github.com>
2023-07-20 02:27:03 -0300

    y

--- backend/src/config.mjs
+++ backend/src/config.mjs
@@ -8,5 +8,10 @@ export default
     dbUser: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
     server: process.env.DB_SERVER || '',
-    dbDataBase: process.env.DB_DATABASE || ''
+    dbDataBase: process.env.DB_DATABASE || '',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
 };
