ab773943c7e07559c3e59b446425d8e09519e789
Agustina-Flores <28354189+Agustina-Flores@users.noreply.github.com>
2023-07-20 05:08:25 -0300

    f

--- backend/src/config.mjs
+++ backend/src/config.mjs
@@ -3,14 +3,10 @@ import {config} from 'dotenv'
 config();
 
 export default {
-  port: process.env.PORT || 4000 ,
+  port: process.env.PORT || 1433 ,
   dbUser: process.env.DB_USER || "",
   dbPassword: process.env.DB_PASSWORD || "",
   dbServer: process.env.DB_SERVER || "",
   dbDataBase: process.env.DB_DATABASE || "",
-  //pool: {
-   // max: 10,
-  //  min: 0,
-  //  idleTimeoutMillis: 30000,
- // },
+ 
 };
